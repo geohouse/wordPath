51d1389bd1f7616799f0d7e417ba73c557361f07
geohouse <geo.house7@gmail.com>
2020-09-29 18:56:20 -0600

    better logging

--- wordPath.js
+++ wordPath.js
@@ -577,10 +577,11 @@ function calculateAnswers(index, currRow, currCol){
 
         } else if(genNum > 1){
             console.log("Testing.");
-            console.log("the genNum is: " + genNum + " the prev. gen num is: " + genNum - 1);
+            console.log("the genNum is: " + genNum);
+            console.log(" the prev. gen num is: " + (genNum - 1));
             prevGenObject = visitTracker_obj[genNum - 1];
             console.log("visitTracker_obj keys are: " + Object.keys(visitTracker_obj));
-            console.log("last genNum is: " + genNum - 1);
+            console.log("last genNum is: " + (genNum - 1));
             console.log("prevGenObject is: " + prevGenObject);
             prevGenKeys = Object.keys(prevGenObject);
             console.log("For genNum: " + genNum + " the keys are: " + prevGenKeys);
